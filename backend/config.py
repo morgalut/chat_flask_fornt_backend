9e179592fb2bc9f5dd920c7d6c9fde958f66b6a6
morgalut <146362490+morgalut@users.noreply.github.com>
2024-07-01 20:21:53 +0300

    add about page

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,7 @@ class Config:
     # Flask app configuration
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     DEBUG = True  # Set to False in production
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///your_database.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://your_user:your_password@db:5432/your_database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Base directory of the project
 
@@ -16,3 +16,4 @@ class Config:
     # File upload configuration
     INSTANCE_FOLDER_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance')
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)),'img')
+
